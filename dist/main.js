587c5e927bfd31f05f0ae1d97e2a1e48d43867be
Queenshiba <haruka0111.2a@gmail.com>
2020-10-10 17:05:54 +0900

    add css

--- dist/main.js
+++ dist/main.js
@@ -1,39 +1,59 @@
-function shuffle(array) {
-  let currentIndex = array.length, temporaryValue, randomIndex;
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
+function convert() {
+  let originalInpput = document.getElementById('originalInpput')
+  let originalInputText = originalInpput.value
+  console.log(typeof originalInputText)
 
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+    let convertedOutput = document.getElementById('convertedOutput')
 
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
+    convertedOutput.value = sentenceShuffler(originalInputText)
+    console.log(convertedOutput.value)
+      
+  function sentenceShuffler(str) {
+
+
+    let regex = /(\w+'\w+)|\w+|,|\.|\!/g
+    // console.log(str)
+    let separetedSigns = str.match(regex);
+
+    return shuffle(separetedSigns);
+
+
   }
 
-  let slashes = array.join(' / ');
-  console.log(slashes)
-  return '( ' + slashes + ' )'
-  
+
+
+
+  function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    let slashes = array.join(' / ');
+    console.log(slashes)
+    return '( ' + slashes + ' )'
+
+  }
+
+
+
 }
 
-
-function sentenceShuffler(str) {
-  console.log(str)
-  let originalInpput = document.getElementById('originalInpput')
-  let originalInpputText = originalInpput.value
-  let regex = /(\w+'\w+)|\w+|,|\.|\!/g
-
-  let separetedSigns = originalInpputText.match(regex);
-  //   console.log(separetedSigns)
-
-  return shuffle(separetedSigns);
-
+function copySentence() {
+let convertedOutput = document.getElementById('convertedOutput')
+convertedOutput.select()
+convertedOutput.setSelectionRange(0,99999)
+document.execCommand("copy");
+console.log(convertedOutput.value)
 
 }
-
-  let ConvertedOutput = document.getElementById('ConvertedOutput')
-// let ConvertedOutputText = sentenceShuffler(str)
